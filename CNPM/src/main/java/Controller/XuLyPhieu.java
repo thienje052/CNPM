f491400c20ccebe68972fceb39d2a75e67baf500
LeDuongAnhTuan <ldat161203@gmail.com>
2025-05-03 22:31:53 +0700

    Them file Xu ly phieu nhap xuat - Tuan

---
+++ CNPM/src/main/java/Controller/XuLyPhieu.java
@@ -0,0 +1,53 @@
+package Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.sql.*;
+
+import Model.LoaiPhieu;
+import Model.Phieu;
+import DAO.DBConnector;
+
+@WebServlet("/tao-phieu")
+public class XuLyPhieu extends HttpServlet {
+	
+	@Override
+    public void init() {
+        try {
+            Connection conn = DBConnector.getConnection();
+            phieuDao = new PhieuDaoImpl(conn);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+	
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            int id = Integer.parseInt(req.getParameter("id"));
+            int idPartner = Integer.parseInt(req.getParameter("id_partner"));
+            int idEmployee = Integer.parseInt(req.getParameter("id_employee"));
+            LoaiPhieu type = LoaiPhieu.valueOf(req.getParameter("type"));
+            LocalDateTime dateTime = LocalDateTime.parse(req.getParameter("datetime"));
+
+            Phieu phieu = new Phieu(id, idPartner, idEmployee, type, dateTime);
+
+            // TODO: Lưu phiếu vào cơ sở dữ liệu (gọi service hoặc DAO tương ứng)
+
+            req.setAttribute("message", "Tạo phiếu thành công!");
+        } catch (Exception e) {
+            req.setAttribute("error", "Lỗi khi tạo phiếu: " + e.getMessage());
+        }
+        req.getRequestDispatcher("/WEB-INF/tao_phieu.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/tao_phieu.jsp").forward(req, resp);
+    }
+}
+
