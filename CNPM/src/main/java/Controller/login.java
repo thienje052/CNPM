b65503951475d23548902663f8bc8be4979eca95
Hao <nhathao0910@gmail.com>
2025-05-07 23:46:50 +0700

    update login, logout

--- CNPM/src/main/java/Controller/login.java
+++ CNPM/src/main/java/Controller/login.java
@@ -21,51 +21,58 @@ public class login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private AuthLogin authLogin;
 	private Connection conn;
-    @Override
-    public void init() {
-        try {
-            conn = DBConnector.getConnectionAuth();
-            authLogin = new AuthLogin(new DAOTaiKhoanNhanVien(conn));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    @Override
+//    public void init() {
+//        try {
+//            conn = DBConnector.getConnectionAuth();
+//            authLogin = new AuthLogin(new DAOTaiKhoanNhanVien(conn));
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-       
-        TaiKhoanNhanVien taiKhoanNhanVien = authLogin.authenticate(username, password);
-        if (taiKhoanNhanVien != null) {
-            HttpSession session = req.getSession();
-            session.setAttribute("currentUser", taiKhoanNhanVien);
-            DAONhanVien daoNhanVien = new DAONhanVien(conn);
-            daoNhanVien.findNVbyID(String.valueOf(taiKhoanNhanVien.getID_Employee())).getPosition().toString();
-            if(daoNhanVien.findNVbyID(String.valueOf(taiKhoanNhanVien.getID_Employee())).getPosition()==ChucVu.Manager) {
-            	try {
-					conn.close();
-					conn = DBConnector.getConnectionForLogin(taiKhoanNhanVien.getUserAccount(), taiKhoanNhanVien.getPassword());
-	                resp.sendRedirect(req.getContextPath() + "/1.sidebar.html");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} else {
-				try {
-					conn.close();
-					conn = DBConnector.getConnectionForLogin(taiKhoanNhanVien.getUserAccount(), taiKhoanNhanVien.getPassword());
-					resp.sendRedirect(req.getContextPath() + "/TKNV_sidebar.html");
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-            }
-        } else {
-            req.setAttribute("error", "Tên đăng nhập hoặc mật khẩu không đúng");
-            req.getRequestDispatcher("/0.login.html").forward(req, resp);
-        }
+		try {
+		      conn = DBConnector.getConnectionAuth();
+		      authLogin = new AuthLogin(new DAOTaiKhoanNhanVien(conn));
+		      String username = req.getParameter("username");
+		        String password = req.getParameter("password");
+		       
+		        TaiKhoanNhanVien taiKhoanNhanVien = authLogin.authenticate(username, password);
+		        if (taiKhoanNhanVien != null) {
+		            HttpSession session = req.getSession();
+		            session.setAttribute("currentUser", taiKhoanNhanVien);
+		            DAONhanVien daoNhanVien = new DAONhanVien(conn);
+		            daoNhanVien.findNVbyID(String.valueOf(taiKhoanNhanVien.getID_Employee())).getPosition().toString();
+		            if(daoNhanVien.findNVbyID(String.valueOf(taiKhoanNhanVien.getID_Employee())).getPosition()==ChucVu.Manager) {
+		            	try {
+							conn.close();
+							conn = DBConnector.getConnectionForLogin(taiKhoanNhanVien.getUserAccount(), taiKhoanNhanVien.getPassword());
+			                resp.sendRedirect(req.getContextPath() + "/1.sidebar.html");
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					} else {
+						try {
+							conn.close();
+							conn = DBConnector.getConnectionForLogin(taiKhoanNhanVien.getUserAccount(), taiKhoanNhanVien.getPassword());
+							resp.sendRedirect(req.getContextPath() + "/TKNV_sidebar.html");
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+		            }
+		        } else {
+		            req.setAttribute("error", "Tên đăng nhập hoặc mật khẩu không đúng");
+		            req.getRequestDispatcher("/0.login.html").forward(req, resp);
+		        }
+		} catch (Exception e) {
+		      throw new RuntimeException(e);
+		}
     }
+        
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
---
+++ CNPM/src/main/java/Controller/logout.java
@@ -0,0 +1,20 @@
+package Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/logout")
+public class logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        resp.sendRedirect(req.getContextPath() + "/0.login.html");
+    }
+}
